ab1e032fd89f89686d8aca46d33a7479558fbaf5
iasonasxrist <iasonasxrist@gmail.com>
2025-04-12 17:32:01 +0300

    Corrected Main

--- Movies/Movies.Api/Program.cs
+++ Movies/Movies.Api/Program.cs
@@ -1,35 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using Movies.Api.Mapping;
 using Movies.Application;
+using Movies.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+// 1. Add CORS services and configure the policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins(
+                    "http://localhost:3000", //frontend 
+                    "http://localhost:8081",
+                    "http://localhost:8080",
+                    "http://your-other-dev-machine:port",
+                    "https://www.your-production-frontend.com"
+                )
+                .AllowAnyHeader() // Allows common headers
+                .AllowAnyMethod() // Allows common HTTP methods (GET, POST, PUT, DELETE, etc.)
+                .AllowCredentials(); // *** Include ONLY if your frontend sends cookies or Auth headers ***
+            // *** If you add this, you CANNOT use AllowAnyOrigin() ***
+        });
+});
+
 var config = builder.Configuration;
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddApplication();
-builder.Services.AddDatabase(config["ShardMapping:Shard1:ConnectionString"]);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
+builder.Services.AddDatabase(config.GetConnectionString("MoviesDb"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var initializer = scope.ServiceProvider.GetRequiredService<DbInitializer>();
+    await initializer.InitializeAsync();
+}
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
 // app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
+app.UseMiddleware<ValidationMappingMiddleware>();
 app.MapControllers();
+
+
+
 
 app.Run();
